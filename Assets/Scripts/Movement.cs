f1d249e1eef097b9f87e3fe28ae249fa1566b4b4
Ian Foster <fosterian95@gmail.com>
2021-12-30 18:29:27 +0000

    Initial Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float vertical_thrust = 1000f;
+    [SerializeField] float rotation_thrust = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust() {
+        //Method for getting spacebar input for accelerating rocket
+        if (Input.GetKey(KeyCode.Space)) {
+            float vertical_force = vertical_thrust * Time.deltaTime;
+            rb.AddRelativeForce(0, vertical_force, 0);
+        }
+    }
+
+    void ProcessRotation() {
+        //Method for checking if player is rotating the rocket and seeing which direction to rotate
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotation_thrust);
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            ApplyRotation(-rotation_thrust);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        float rotation_force = rotationThisFrame * Time.deltaTime;
+        transform.Rotate(0, 0, rotation_force);
+        rb.freezeRotation = false;
+    }
+}
