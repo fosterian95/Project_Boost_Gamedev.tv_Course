ce17f589d268abf2b0ebf3584d2f131ffbebfa02
Ian Foster <fosterian95@gmail.com>
2022-05-21 17:29:17 +0100

    Altered blue obstacle course to include movement on blue obstacles

--- Assets/Scripts/BlueObstacleMovement.cs
+++ Assets/Scripts/BlueObstacleMovement.cs
@@ -7,6 +7,8 @@ public class BlueObstacleMovement : MonoBehaviour
     [SerializeField] float yAxisMovement = 0.01f;
     [SerializeField] int upperBounds = 10;
     [SerializeField] int lowerBounds = 1;
+
+    int movementDirection = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,21 +18,17 @@ public class BlueObstacleMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ChangeMovementDirection();
-        ObstacleMotion();
-    }
-
-    int ChangeMovementDirection() {
-        int movementDirection = 1;
-        if ((transform.position.y > upperBounds) || (transform.position.y < lowerBounds)) {
-            movementDirection *= -1;
+        if (transform.position.y >= upperBounds) {
+            movementDirection = -1;
+        } else if (transform.position.y <= lowerBounds) {
+            movementDirection = 1;
         }
-        return movementDirection;
+        Debug.Log(movementDirection);
+        ObstacleMotion(movementDirection);
     }
-    void ObstacleMotion() {
-        int moveUpOrDown = ChangeMovementDirection();
-        float yAxisMovementSpeed = yAxisMovement * Time.deltaTime;
-        float actualObstacleMotion = yAxisMovementSpeed * moveUpOrDown;
-        transform.Translate(0, actualObstacleMotion, 0);
+    
+    void ObstacleMotion(int direction) {
+        float yAxisMovementSpeed = yAxisMovement * Time.deltaTime * direction;
+        transform.Translate(0, yAxisMovementSpeed, 0);
     }
 }
